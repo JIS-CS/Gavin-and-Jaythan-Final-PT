8e4f9f2e0a54d2b3d9bb5f89bd4d254763da13b9
gavinnlimm <65484@jisedu.or.id>
2023-02-01 15:06:17 +0700

    1/02 - Init

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-
 # Plague.inc
 
 Developers:
 - Gavin Lim (65484)
 - Jaythaneal Skylar Sutrisno (65720)
+
+Description:
---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Starting point 90% deaths
+
+// We see how many deaths we end u with after changin gthe circumstances
+
+function startPop {
+    
+}
+
+function simulate() {
+    
+}
