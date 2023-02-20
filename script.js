81f9a85f7f5d62ed452f9bfe8ad8851ab0936da5
gavinnlimm <65484@jisedu.or.id>
2023-02-20 12:57:18 +0700

    test

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 function calculations() {
     
 
-    let x1 = (singlePly * 0,5) + (n95Mask * 1) + (vaccinationValue * 2) + (socialValue * 0,6);
+    let x1 = (singlePly * 0,5) + (n95Mask * 1) + (vaccinationValue * 2);
 }
 
 function simulate() {
@@ -13,15 +13,17 @@ function simulate() {
     let singlePly = parseInt(document.getElementById("singlePlyMasks").value);
     let vaccinationvalue = parseInt(document.getElementById("n95Masks").value);
     let freqVac = parseInt(document.getElementById("freqVac").value);
+
     let amountSurvivors = startPop * 0.1;
     let amountDeaths = startPop * 0.9;
     let masks1 = singlePly * 0.1;
     let vaccines = vaccinationvalue * 0.5;
     
-
+    
     // Without any changes to the factors, 90% of the population dies from the virus
+    let finalSurvivors = amountSurvivors + masks1 + vaccines;
 
-    document.getElementById("survivors").innerHTML += amountSurvivors + masks1 + vaccines;
+    document.getElementById("survivors").innerHTML += finalSurvivors;
 
     document.getElementById("deaths").innerHTML += amountDeaths;
 }
