6053ecd7fcc74c052ded40f870f5d37dbd473633
gavinnlimm <65484@jisedu.or.id>
2023-02-22 11:28:18 +0700

    22 Feb - Fixed the simulate function in javascript to allow program to function properly

--- script.js
+++ script.js
@@ -1,63 +1,30 @@
-// Global Variables
-
-// Starting population variable
-
-function simulate() {
-    let finalStartPop = parseInt(document.getElementById("startPop").value) * 0.1;
-
-    // Inital finalSurvivors value; regardless of factors, finalSurvivors = 10% of startPop
-    let finalSurvivors = parseInt(finalStartPop * 0.1);
-
-    // Factor of success rate of living for vaccine: 100%
-    // let freqVac = parseInt(document.getElementById("freqVac").value);
-
-    // let finalSurvivors = parseInt(finalSurvivors + freqVac);
-
-    // let singlePlyMasks = parseInt(document.getElementById("singlePlyMasks").value); 
-    // let n95Masks = parseInt(document.getElementById("n95Masks").value);
-
-    document.getElementById("survivors").innerHTML += finalSurvivors;
-    document.getElementById("deaths").innerHTML += finalDeaths;
-}
-
-// function simulatePandemic() {
-//     let startPop = parseInt(document.getElementById("startPop").value);
-//     let totalSurvivors = 0;
-    
-//     // Simulate the spread of the pandemic
-//     while (infected > 0) {
-//       const newInfections = Math.ceil(infected * 0.2);
-//       infected += newInfections;
-//       const deaths = Math.floor(infected * 0.05);
-//       infected -= deaths;
-//       totalSurvivors += (infected - deaths);
-//     }
-    
-//     // Print the final number of survivors
-//     const finalSurvivors = Math.floor(startPop * 0.1);
-//     console.log(`Out of the original ${startPop} population, only ${finalSurvivors} survived the pandemic.`);
-    
-//     return totalSurvivors;
-//   }
-
 function simulatePandemic() {
-    const startPop = parseInt(document.getElementById("startPop").value);
-    let infected = startPop;
-    let totalSurvivors = 0;
-    let totalDeaths = 0;
-  
-    while (infected > 0) {
-      const newInfections = Math.ceil(infected * 0.2);
-      infected += newInfections;
-      const deaths = Math.floor(infected * 0.05);
-      infected -= deaths;
-      totalDeaths += deaths;
-      totalSurvivors += (infected - deaths);
-    }
-  
-    const finalSurvivors = Math.floor(startPop * 0.1);
-    const finalDeaths = totalDeaths + (startPop - totalSurvivors - totalDeaths);
-    
-    document.getElementById("survivors").innerHTML += finalSurvivors;
-    document.getElementById("deaths").innerHTML += finalDeaths;
+  let startPop = parseInt(document.getElementById("startPop").value);
+  let singlePlyMasks = parseInt(document.getElementById("singlePlyMasks").value);
+  let n95Masks = parseInt(document.getElementById("n95Masks").value);
+  let freqVac = parseFloat(document.getElementById("freqVac").value);
+
+  // Validate user input
+  if (isNaN(startPop) || isNaN(singlePlyMasks) || isNaN(n95Masks) || isNaN(freqVac)) {
+    alert("Please enter a valid number for all input fields.");
+    return;
   }
+
+  // Calculate the number of deaths
+  let totalDeaths = startPop * 0.9;
+  let maskedDeaths = (singlePlyMasks * 0.4) + (n95Masks * 0.4);
+  let unmaskedDeaths = (totalDeaths - maskedDeaths) * (1 - freqVac) * 0.1;
+  let totalDeathsAfterVaccine = maskedDeaths + unmaskedDeaths;
+
+  // Calculate the number of survivors
+  let survivors = startPop - totalDeathsAfterVaccine;
+
+  // If the number of survivors is negative, set it to 0
+  if (survivors < 0) {
+    survivors = 0;
+  }
+
+  // Update the HTML with the results
+  document.getElementById("deaths").innerHTML = survivors;
+  document.getElementById("survivors").innerHTML = totalDeathsAfterVaccine;
+}
